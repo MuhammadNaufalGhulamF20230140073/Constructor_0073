c69e2ab33abbf7280a65720d32536a115677b8c7
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-13 14:47:44 +0700

    membuat fungsi untuk mencetak id dan nama

--- contoh1.cpp
+++ contoh1.cpp
@@ -23,4 +23,14 @@ void mahasiswa::setID()
     id = nim++;
 }
 
+void mahasiswa ::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+
+}
+
+}
+
 
