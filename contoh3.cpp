830853103325b8e1721b223630552e3bc820910f
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-13 15:34:22 +0700

    membuat class mahasiswa

--- contoh3.cpp
+++ contoh3.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = iNim;
+    };
+
+};
